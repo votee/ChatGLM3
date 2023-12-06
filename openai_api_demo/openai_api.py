53d9463d12d9bd16cd284d0491c0917c60acf39b
King Ho <hosaipang@gmail.com>
2023-12-06 17:28:09 +0800

    Debug on crash.

--- openai_api_demo/openai_api.py
+++ openai_api_demo/openai_api.py
@@ -194,7 +194,8 @@ async def predict(model_id: str, params: dict):
         finish_reason=None
     )
     chunk = ChatCompletionResponse(model=model_id, choices=[choice_data], object="chat.completion.chunk")
-    yield "{}".format(chunk.model_dump_json(exclude_unset=True))
+    # yield "{}".format(chunk.model_dump_json(exclude_unset=True))
+    yield "{}".format(chunk.json(exclude_unset=True))
 
     previous_text = ""
     for new_response in generate_stream_chatglm3(model, tokenizer, params):
